e2af470b1e006f9ff7771c2ce910e3f93598dd57
ridvanuyan <ridvan.uyan@togg.com.tr>
2023-10-05 11:34:09 +0300

    Added new json

--- index.js
+++ index.js
@@ -4,9 +4,47 @@ const PORT = 3000;
 
 app.get('/big-json', (req, res) => {
     const bigJson = {
-        key1: "value1",
-        key2: "value2",
-    };
+
+            "loloId": "XXXXX",
+            "name": "Use4ds",
+            "walletId": "0x32132132132",
+            "balance": {
+                "avaxBalance": {
+                    "totalAmount": "0",
+                    "availableAmount": "0",
+                    "reservedAmount": "0",
+                    "quarantinedAmount": "0"
+                },
+                "toggensBalance": {
+                    "totalAmount": "0",
+                    "availableAmount": "0",
+                    "reservedAmount": "0",
+                    "quarantinedAmount": "0"
+                }
+            },
+            "affiliate": false,
+            "showWhitelisting": true,
+            "showConditions": true,
+            "creationDate": "2023-08-10T08:02:08.359Z",
+            "modificationDate": "2023-08-10T08:42:49.333Z",
+            "kycVerificationLevel": "UNSPECIFIED",
+            "kycExpirationDate": "2023-08-20T08:02:08.243Z",
+            "locale": "TR",
+            "whitelisting": "NOT_WHITELISTED",
+            "customerBalance": {
+                "accID": "3233131321",
+                "tokenBalances": [
+                    {
+                        "amount": 0,
+                        "name": "Avalanche",
+                        "symbol": "AVAX",
+                        "address": "0x0000000000000000000000000000000000000000",
+                        "date": "2023-08-10T10:45:18.344Z"
+                    }
+                ]
+            }
+        }
+    ;
     res.json(bigJson);
 });
 
